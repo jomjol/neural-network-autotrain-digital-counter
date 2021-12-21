5f21fc131c470bdcdb186199ac469aff7e9309b4
jomjol <30766535+jomjol@users.noreply.github.com>
2021-12-21 07:48:06 +0100

    Update

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ for x in configurations:
         print("Das ist in Script - Teil 1: " + str(arg1))
         a_script = open("script_automate/doTraining.py").read()
         sys.argv = ["script_automate/doTraining.py", arg1]
-    exec(a_script)
+        exec(a_script)
     print("Das ist in Script - Teil 2: " + str(arg2))
     b_script = open("script_automate/doReport.py").read()
     sys.argv = ["script_automate/doReport.py", arg2]

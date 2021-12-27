9b8e2d2ca4fd1b8f0f51623a0c5beb85e52f86e6
jomjol <30766535+jomjol@users.noreply.github.com>
2021-12-27 10:46:40 +0100

    Update script.py

--- c_scripts/script.py
+++ c_scripts/script.py
@@ -8,19 +8,19 @@ if LogFile:
         os.remove(LogFile)
 
 if not ReportOnly:
-    exec(open("a_scripts/script_automate/image_preparation.py").read())
+    exec(open("script_automate/image_preparation.py").read()))
     
 for x in configurations:
     arg1 = x
     arg2 = x
     if not ReportOnly:
 #        print("Das ist in Script - Teil 1: " + str(arg1))
-        a_script = open("a_scripts/script_automate/doTraining.py").read()
-        sys.argv = ["a_scripts/script_automate/doTraining.py", arg1]
+        a_script = open("../c_scripts/script_automate/doTraining.py").read()
+        sys.argv = ["../c_scripts/script_automate/doTraining.py", arg1]
         exec(a_script)
 #    print("Das ist in Script - Teil 2: " + str(arg2))
-    b_script = open("a_scripts/script_automate/doReport.py").read()
-    sys.argv = ["a_scripts/script_automate/doReport.py", arg2]
+    b_script = open("../c_scripts/script_automate/doReport.py").read()
+    sys.argv = ["../c_scripts/script_automate/doReport.py", arg2]
     exec(b_script)
     
 sys.stdout = original_stdout
